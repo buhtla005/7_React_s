217c9fd39174494f41333d6362ef54acf7885c1f
unknown <noobprogramer@yahoo.com>
2020-01-26 02:18:01 +0100

    First commit

---
+++ learn-react/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <div>
+            <footer class="blockquote-footer">
+                <small class="text-muted">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+                </small>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
